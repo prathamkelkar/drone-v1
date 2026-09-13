a5c5527a033938c5d77d51b4b8e61a383cd299c2
prathamkelkar <pratham.kel@gmail.com>
2026-09-13 20:09:04 +1000

    Initial image processing

--- perception/perception/perception_node.py
+++ perception/perception/perception_node.py
@@ -3,8 +3,27 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from vision_msgs.msg import Detection2DArray
+
 class PerceptionNode(Node):
     def __init__(self):
         super().__init__('perception_node')
 
-        
+        qos = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        self.image_sub = self.create_subscription(Image, '/camera/image_raw', self.image_callback, qos)
+
+    def image_callback(self, msg):
+        try:
+            bridge = CvBridge()
+            cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            self.get_logger().info('Received image of size: {}x{}'.format(cv_image.shape[1], cv_image.shape[0]))
+        except CvBridgeError as e:
+            self.get_logger().error('CvBridge Error: {}'.format(e))
